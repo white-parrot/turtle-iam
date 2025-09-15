c42e6a101504050e9aa76ad3e78a2f19efaa6598
shreyansh-p <shreyansh.p@copperpoddigital.com>
2025-09-15 17:30:38 +0530

    Initial Base Code Committed

---
+++ src/test/java/rt/turtleIam/TurtleIamApplicationTests.java
@@ -0,0 +1,13 @@
+package rt.turtleIam;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TurtleIamApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
